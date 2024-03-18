7f0302596bda4ef421691384df796e65d92135cf
Mehmet Kaplan <mehmetkaplanse@gmail.com>
2024-03-18 16:06:17 +0300

    using nlog

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -12,15 +12,14 @@ namespace Services
     public class BookManager : IBookService
     {
         private readonly IRepositoryManager _manager;
-        public BookManager(IRepositoryManager manager)
+        private readonly ILoggerService _logger;
+        public BookManager(IRepositoryManager manager, ILoggerService logger)
         {
-            _manager = manager; 
+            _manager = manager;
+            _logger = logger;
         }
         public Book CreateOneBook(Book book)
         {
-            if(book is null)    
-                throw new ArgumentNullException(nameof(book)); 
-
             _manager.Book.CreateOneBook(book);
             _manager.Save();
             return book;    
@@ -30,7 +29,12 @@ namespace Services
         {
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
             if (entity is null)
-                throw new Exception($"Book with id:{id} could not found!");
+            {
+                string message = $"Book with id:{id} could not found!";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
+                
 
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();    
@@ -49,11 +53,15 @@ namespace Services
         public void UpdateOneBook(int id, Book book, bool trackChanges)
         {
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
-            if(entity is null)
-                throw new Exception($"Book with id:{id} could not found!");
+            if (entity is null)
+            {
+                string message = $"Book with id:{id} could not found!";
+                _logger.LogInfo(message);
+                throw new Exception(message);
+            }
 
             // checked params
-            if(book is null)
+            if (book is null)
                 throw new ArgumentNullException(nameof(book));
 
             entity.Title = book.Title;
